fb7b770cf11548f6dae3567e7620429a9791d4fc
Hemanth B <hemanth22hemu@gmail.com>
2021-06-30 06:13:07 +0530

    Update Cucun.py

--- Cucun.py
+++ Cucun.py
@@ -1,7 +1,9 @@
 from forex_python.converter import CurrencyRates
+rom forex_python.converter import CurrencyCodes
 c = CurrencyRates()
+s = CurrencyCodes()
 a = c.get_rate('INR','SGD')
-print('singapure currency: {0} {1}'.format((a*1362000),c.get_symbol('SGD')))
+print('singapure currency: {0} {1}'.format((a*1362000),s.get_symbol('SGD')))
 d = c.get_rate('INR','EUR')
-print('European Currency: {0} {1}'.format((d*1362000),c.get_symbol('EUR')))
-print('European Currency: {0} {1}'.format((d*853000),c.get_symbol('EUR')))
+print('European Currency: {0} {1}'.format((d*1362000),s.get_symbol('EUR')))
+print('European Currency: {0} {1}'.format((d*853000),s.get_symbol('EUR')))
