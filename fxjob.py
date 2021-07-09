974e985695aace49fa13c50ed09de359c62d5ea8
Hemanth B <hemanth22hemu@gmail.com>
2021-07-09 10:28:29 +0530

    Update fxjob.py

--- fxjob.py
+++ fxjob.py
@@ -1,7 +1,7 @@
 from forex_python.converter import CurrencyRates
 import json
 c = CurrencyRates()
-abc = c.get_rates('USD')
+abc = c.get_rates('INR')
 dxg = json.dumps(abc,sort_keys=True, indent=4)
 #print(abc)
 for i,z in abc.items():
